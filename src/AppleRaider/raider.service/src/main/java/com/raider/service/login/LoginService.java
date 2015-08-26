c6330bc9b9b0550514dbdc8e5cecb76f423ce349
69025318 <69025318@qq.com>
2015-08-26 17:17:10 +0800

    java code commit

--- src/AppleRaider/raider.service/src/main/java/com/raider/service/login/LoginService.java
+++ src/AppleRaider/raider.service/src/main/java/com/raider/service/login/LoginService.java
@@ -1,12 +1,21 @@
 package com.raider.service.login;
 
 
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
 /**
  * 实现苹果官网自动登录
  *
  * @author wenchun
  */
+@Service
 public class LoginService {
+    private Logger logger = Logger.getLogger(LoginService.class.toString());
 
+    public static void testLogin(String userName, String password){
+
+    }
 
 }
---
+++ src/AppleRaider/raider.web/src/main/java/com/raider/web/controller/smsController.java
@@ -0,0 +1,7 @@
+package com.raider.web.controller;
+
+/**
+ * Created by wenchun on 2015/8/25.
+ */
+public class smsController {
+}
